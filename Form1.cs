36e0f3a43f294e3c06c8dc957bb2052019b24442
afiseleo <aaaa50306@gmail.com>
2025-05-10 16:34:08 +0800

    新增專案和主要功能檔案
    新增 `BlueArchiveGUIDownloader` 專案檔和解決方案檔，設定 SDK、輸出類型及目標框架。新增 `MainForm` 類別及其 UI 設置，並實現日誌輸出功能。新增本地化字串檔案以支援繁體中文。新增自定義的 `RichTextBoxWriter` 以便於將文字輸出到 `RichTextBox` 控制項。管理應用程式資源的相關檔案也已新增。

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+using System.Text.RegularExpressions;
+
+namespace BlueArchiveGUIDownloader
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Console.SetOut(new RichTextBoxWriter(logbox));
+            Console.WriteLine("Blue Archive GUI Downloader");
+            VersionText.ValidatingType = typeof(string);
+            VersionText.TypeValidationCompleted += VersionText_TypeValidationCompleted;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (UseChromeBrowserDownload.Checked)
+            {
+                DirectDownload.Checked = false;
+            }
+
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (DirectDownload.Checked)
+            {
+                UseChromeBrowserDownload.Checked = false;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VersionText_TypeValidationCompleted(object sender, TypeValidationEventArgs e)
+        {
+            var tb = sender as MaskedTextBox;
+            if (tb == null) return;
+
+            // 驗證結果：失敗就紅底，成功就白底
+            tb.BackColor = e.IsValidInput
+                ? Color.Green
+                : Color.Red;
+        }
+        
+    }
+}
